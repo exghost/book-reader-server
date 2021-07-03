92675a5758784bb1e980d2e6b3bb03ebff748973
exghost <robertlucas198@gmail.com>
2021-07-02 21:29:02 -0400

    setup user in graphql

--- src/index.js
+++ src/index.js
@@ -1,36 +1,10 @@
 const { ApolloServer, gql } = require('apollo-server');
 
-const typeDefs = gql`
-    type Book {
-        title: String
-        author: String
-    }
-
-    type Query {
-        books: [Book]
-    }
-`;
-
-const books = [
-    {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
-    },
-    {
-        title: 'City of Glass',
-        author: 'Paul Auster',
-    }
-];
-
-const resolvers = {
-    Query: {
-        books: () => books,
-    }
-}
+const { userResolvers, userTypeDefs } = require('./users');
 
 const server = new ApolloServer({ 
-    typeDefs, 
-    resolvers,
+    typeDefs: userTypeDefs, 
+    resolvers: userResolvers,
     mockEntireSchema: false
 });
 
